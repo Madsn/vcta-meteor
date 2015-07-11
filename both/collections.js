a0e97298d79e71255cfc7e9504839791b934dec3
Mikkel Madsen <m@madsn.net>
2015-07-11 13:49:01 +0200

    Add orion admin

--- both/collections.js
+++ both/collections.js
@@ -1,1 +1,12 @@
-Players = new Meteor.Collection('players');
+Players = new orion.collection('players', {
+  singularName: 'player',
+  pluralName: 'players',
+  link: {
+    title: 'Players'
+  },
+  tabular: {
+    columns: [
+      { data: 'name', title: 'Name' }
+    ]
+  }
+})
--- both/tables.js
+++ both/tables.js
@@ -5,7 +5,5 @@ Meteor.isClient && Template.registerHelper('TabularTables', TabularTables);
 TabularTables.Players = new Tabular.Table({
   name: "PlayerList",
   collection: Players,
-  columns: [
-    {data: "name", title: "Name"}
-  ]
+  columns: Players.tabular.columns
 });
