c8f570419ad0f592cf965f121c22f2e49e760a49
Mikkel Madsen <m@madsn.net>
2015-07-20 21:44:35 +0200

    Add trips update hook

--- server/hooks.js
+++ server/hooks.js
@@ -7,13 +7,31 @@ Teams.after.remove(function (userId, doc) {
 });
 
 Trips.after.insert(function (userId, doc) {
-  var incrementCyclingDay = Trips.findOne({userId: doc.userId, date: doc.date}) ? 0 : 1;
-  Meteor.users.update({_id: doc.userId}, {$inc: {cyclingDays: incrementCyclingDay, distance: doc.distance}});
+  var incrementCyclingDay = Trips.findOne({_id: {$ne: doc._id},
+    userId: doc.userId, date: doc.date}) ? 0 : 1;
+  Meteor.users.update({_id: doc.userId},
+    {$inc: {cyclingDays: incrementCyclingDay, distance: doc.distance}});
 });
 
 Trips.after.remove(function (userId, doc) {
-  var incrementCyclingDay = Trips.findOne({userId: doc.userId, date: doc.date, _id: {$ne: doc._id}}) ? 0 : 1;
-  Meteor.users.update({_id: doc.userId}, {$inc: {cyclingDays: -incrementCyclingDay, distance: -doc.distance}});
+  var incrementCyclingDay = Trips.findOne({_id: {$ne: doc._id},
+    userId: doc.userId, date: doc.date}) ? 0 : 1;
+  Meteor.users.update({_id: doc.userId},
+    {$inc: {cyclingDays: -incrementCyclingDay, distance: -doc.distance}});
+});
+
+Trips.after.update(function (userId, doc) {
+  var cyclingDaysDiff = 0;
+  var distanceDiff = doc.distance - this.previous.distance;
+  if (this.previous.date !== doc.date) {
+    var newDateIsUnique = Trips.findOne({userId: doc.userId,
+      date: doc.date, _id: {$ne: doc._id}}) ? 0 : 1;
+    var oldDateIsUnique = Trips.findOne({userId: doc.userId,
+      date: this.previous.date, _id: {$ne: doc._id}}) ? 0 : 1;
+    cyclingDaysDiff = newDateIsUnique - oldDateIsUnique;
+  }
+  Meteor.users.update({_id: doc.userId},
+    {$inc: {cyclingDays: cyclingDaysDiff, distance: distanceDiff}});
 });
 
 Accounts.onCreateUser(function(options, user) {
