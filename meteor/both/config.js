e7d91c33b6bdb50e1cfd7290e3b3b48685e0ab78
Madsn <m@madsn.net>
2015-08-28 20:44:22 +0200

    Add banner to top of pages via orion dictionary

--- meteor/both/config.js
+++ meteor/both/config.js
@@ -2,38 +2,50 @@ if (Meteor.isClient){
   SimpleSchema.debug = true;
 }
 
-orion.config.add('email address', 'config', {
+orion.config.add('EMAIL_ADDR', 'server', {
                     type: String,
                     label: 'Email address used as sender',
                     public: false
                 });
 
-orion.config.add('team management', 'config', {
-                    type: Boolean,
-                    label: 'Should team management be active?',
-                    public: true
-                });
-
-orion.config.add('start date', 'config', {
-                    type: Date,
-                    label: 'First date of the contest',
-                    public: true
-                });
-
-orion.config.add('end date', 'config', {
-                    type: Date,
-                    label: 'Last date of the contest',
-                    public: true
-                });
-
-orion.config.add('endomondo service url', 'config', {
+orion.config.add('ENDOMONDO_SERVICE_URL', 'server', {
                     type: String,
                     label: 'URL to the endomondo sync service',
                     public: false
                 });
 
-orion.config.add('signup enabled', 'config', {
+orion.config.add('TEAM_MANAGEMENT', 'vcta settings', {
+                    type: Boolean,
+                    label: 'Should team management be active?',
+                    public: true
+                });
+
+orion.config.add('START_DATE', 'vcta settings', {
+                    type: Date,
+                    label: 'First date of the contest',
+                    public: true
+                });
+
+orion.config.add('END_DATE', 'vcta settings', {
+                    type: Date,
+                    label: 'Last date of the contest',
+                    public: true
+                });
+
+orion.config.add('SIGNUP_ENABLED', 'vcta settings', {
                     type: Boolean,
                     label: 'Creation of new users enabled?',
-                    public: false
+                    public: true
                 });
+
+orion.dictionary.addDefinition('message', 'banner', {
+    type: String,
+    label: 'Banner message'
+});
+
+orion.dictionary.addDefinition('class', 'banner', {
+    type: String,
+    label: 'Bootstrap class for banner message (danger, warning, info, success)',
+});
+
+Options.set('forbidClientAccountCreation', orion.config.get('SIGNUP_ENABLED'));
